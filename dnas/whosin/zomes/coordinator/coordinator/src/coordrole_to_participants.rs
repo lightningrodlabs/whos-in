26c608a87c0594bef3629dd250f51560d0fecf8a
Leo <leobensman@gmail.com>
2023-03-24 15:56:47 -0400

    validate that agents can't commit other agents

--- dnas/whosin/zomes/coordinator/coordinator/src/coordrole_to_participants.rs
+++ dnas/whosin/zomes/coordinator/coordinator/src/coordrole_to_participants.rs
@@ -5,24 +5,24 @@ pub struct AddParticipantForCoordroleInput {
     coordrole_hash: ActionHash,
     participant: AgentPubKey,
 }
-#[hdk_extern]
-pub fn add_participant_for_coordrole(
-    input: AddParticipantForCoordroleInput,
-) -> ExternResult<()> {
-    create_link(
-        input.coordrole_hash.clone(),
-        input.participant.clone(),
-        LinkTypes::CoordroleToParticipants,
-        (),
-    )?;
-    create_link(
-        input.participant,
-        input.coordrole_hash,
-        LinkTypes::ParticipantToCoordroles,
-        (),
-    )?;
-    Ok(())
-}
+// #[hdk_extern]
+// pub fn add_participant_for_coordrole(
+//     input: AddParticipantForCoordroleInput,
+// ) -> ExternResult<()> {
+//     create_link(
+//         input.coordrole_hash.clone(),
+//         input.participant.clone(),
+//         LinkTypes::CoordroleToParticipants,
+//         (),
+//     )?;
+//     create_link(
+//         input.participant,
+//         input.coordrole_hash,
+//         LinkTypes::ParticipantToCoordroles,
+//         (),
+//     )?;
+//     Ok(())
+// }
 #[hdk_extern]
 pub fn commit_to_coordrole(coordrole_hash: ActionHash) -> ExternResult<()> {
     let participant: AgentPubKey = agent_info()?.agent_latest_pubkey.into();
@@ -95,30 +95,30 @@ pub struct RemoveParticipantForCoordroleInput {
     coordrole_hash: ActionHash,
     participant: AgentPubKey,
 }
-#[hdk_extern]
-pub fn remove_participant_for_coordrole(
-    input: RemoveParticipantForCoordroleInput,
-) -> ExternResult<()> {
-    let links = get_links(
-        input.coordrole_hash.clone(),
-        LinkTypes::CoordroleToParticipants,
-        None,
-    )?;
-    for link in links {
-        if AgentPubKey::from(EntryHash::from(link.target.clone())).eq(&input.participant)
-        {
-            delete_link(link.create_link_hash)?;
-        }
-    }
-    let links = get_links(
-        input.participant.clone(),
-        LinkTypes::ParticipantToCoordroles,
-        None,
-    )?;
-    for link in links {
-        if ActionHash::from(link.target.clone()).eq(&input.coordrole_hash) {
-            delete_link(link.create_link_hash)?;
-        }
-    }
-    Ok(())
-}
+// #[hdk_extern]
+// pub fn remove_participant_for_coordrole(
+//     input: RemoveParticipantForCoordroleInput,
+// ) -> ExternResult<()> {
+//     let links = get_links(
+//         input.coordrole_hash.clone(),
+//         LinkTypes::CoordroleToParticipants,
+//         None,
+//     )?;
+//     for link in links {
+//         if AgentPubKey::from(EntryHash::from(link.target.clone())).eq(&input.participant)
+//         {
+//             delete_link(link.create_link_hash)?;
+//         }
+//     }
+//     let links = get_links(
+//         input.participant.clone(),
+//         LinkTypes::ParticipantToCoordroles,
+//         None,
+//     )?;
+//     for link in links {
+//         if ActionHash::from(link.target.clone()).eq(&input.coordrole_hash) {
+//             delete_link(link.create_link_hash)?;
+//         }
+//     }
+//     Ok(())
+// }
--- dnas/whosin/zomes/integrity/coordinator/src/coordrole_to_participants.rs
+++ dnas/whosin/zomes/integrity/coordinator/src/coordrole_to_participants.rs
@@ -2,7 +2,7 @@ use hdi::prelude::*;
 pub fn validate_create_link_coordrole_to_participants(
     _action: CreateLink,
     base_address: AnyLinkableHash,
-    _target_address: AnyLinkableHash,
+    target_address: AnyLinkableHash,
     _tag: LinkTag,
 ) -> ExternResult<ValidateCallbackResult> {
     let action_hash = ActionHash::from(base_address);
@@ -16,20 +16,41 @@ pub fn validate_create_link_coordrole_to_participants(
                 WasmErrorInner::Guest(String::from("Linked action must reference an entry"))
             ),
         )?;
+    if target_address != record.signed_action.hashed.author().clone().into() {
+        return Ok(ValidateCallbackResult::Invalid(
+            "Only the author of the Coordrole can link to it".into(),
+        ));
+    }
     Ok(ValidateCallbackResult::Valid)
 }
 pub fn validate_delete_link_coordrole_to_participants(
     _action: DeleteLink,
     _original_action: CreateLink,
-    _base: AnyLinkableHash,
-    _target: AnyLinkableHash,
+    base_address: AnyLinkableHash,
+    target_address: AnyLinkableHash,
     _tag: LinkTag,
 ) -> ExternResult<ValidateCallbackResult> {
+    let action_hash = ActionHash::from(base_address);
+    let record = must_get_valid_record(action_hash)?;
+    let _coordrole: crate::Coordrole = record
+        .entry()
+        .to_app_option()
+        .map_err(|e| wasm_error!(e))?
+        .ok_or(
+            wasm_error!(
+                WasmErrorInner::Guest(String::from("Linked action must reference an entry"))
+            ),
+        )?;
+    if target_address != record.signed_action.hashed.author().clone().into() {
+        return Ok(ValidateCallbackResult::Invalid(
+            "Only the author of the Coordrole can link to it".into(),
+        ));
+    }
     Ok(ValidateCallbackResult::Valid)
 }
 pub fn validate_create_link_participant_to_coordroles(
     _action: CreateLink,
-    _base_address: AnyLinkableHash,
+    base_address: AnyLinkableHash,
     target_address: AnyLinkableHash,
     _tag: LinkTag,
 ) -> ExternResult<ValidateCallbackResult> {
@@ -44,14 +65,35 @@ pub fn validate_create_link_participant_to_coordroles(
                 WasmErrorInner::Guest(String::from("Linked action must reference an entry"))
             ),
         )?;
+    if base_address != record.signed_action.hashed.author().clone().into() {
+        return Ok(ValidateCallbackResult::Invalid(
+            "Only the author of the Coordrole can link to it".into(),
+        ));
+    }
     Ok(ValidateCallbackResult::Valid)
 }
 pub fn validate_delete_link_participant_to_coordroles(
     _action: DeleteLink,
     _original_action: CreateLink,
-    _base: AnyLinkableHash,
-    _target: AnyLinkableHash,
+    base_address: AnyLinkableHash,
+    target_address: AnyLinkableHash,
     _tag: LinkTag,
 ) -> ExternResult<ValidateCallbackResult> {
+    let action_hash = ActionHash::from(target_address);
+    let record = must_get_valid_record(action_hash)?;
+    let _coordrole: crate::Coordrole = record
+        .entry()
+        .to_app_option()
+        .map_err(|e| wasm_error!(e))?
+        .ok_or(
+            wasm_error!(
+                WasmErrorInner::Guest(String::from("Linked action must reference an entry"))
+            ),
+        )?;
+    if base_address != record.signed_action.hashed.author().clone().into() {
+        return Ok(ValidateCallbackResult::Invalid(
+            "Only the author of the Coordrole can link to it".into(),
+        ));
+    }
     Ok(ValidateCallbackResult::Valid)
 }
